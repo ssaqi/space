fc2e339e62bcfa13c799a029204dad8efb549ec4
Muhammad Saqib <89692631+ssaqi@users.noreply.github.com>
2023-04-07 19:42:45 +0500

    Update App.js
    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Sign from './Components/Sign/Sign.jsx';
 import {Route , Routes} from 'react-router-dom';
 import ResentImg from './Components/Resend/ResentImg';
 import LoginImg from './Components/Login/LoginImg';
 import Dashhboard from './Components/Dashboard/Dashhboard';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
      <>
    
      <Routes>
-      <Route path='/' element={<Sign/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign' element={<Sign/>}/>
       <Route path='/resent' element={<ResentImg/>}/>
       <Route path='/login' element={<LoginImg/>}/>
       <Route path='/Dashboard' element={<Dashhboard/>}/>
